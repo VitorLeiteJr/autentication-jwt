d8ff104d38d292697dac6b283471719577739a4a
VitorLeiteJr <96vitorleite@gmail.com>
2024-09-04 15:40:33 -0300

    feat: ajust autentication in login with jtw

---
+++ app/api/login/route.ts
@@ -0,0 +1,48 @@
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"
+
+export interface User {
+    id: number;
+    email: string;
+    password: string; 
+  }
+  
+  export const users: User[] = [
+    {
+      id: 1,
+      email: 'vitor',
+      password: '$2a$12$jiyRzpOkkC/XBqo5y1FQI..BsLovQrl95HKitMPaeYIls0C9lHcdO', // bcrypt hash for 'password123'
+    },
+
+  ];
+
+
+export async function POST (req: NextRequest, res: NextResponse) {
+
+         
+    const body = await req.json();
+    const {email, pw } = body;
+
+
+  const user = users.find((user)=>user.email===email);
+
+  if(!user){
+    return NextResponse.json({Error: "user not found"});
+  }
+
+  const passwordCheck = await bcrypt.compare(pw,user.password);
+  
+  if (!passwordCheck){
+    return NextResponse.json({Error: "Password dont match"});
+  }
+
+  const token = jwt.sign({id:user.id, email: user.email}, 
+    process.env.JWT_SECRET as string,
+  { 
+    expiresIn: '1h'
+  });
+
+return NextResponse.json({token: token})
+   
+}
